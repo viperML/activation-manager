7d80871e26a04bd35314f71090f8489f5bc60e77
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-04-17 09:27:19 +0200

    sample rune

--- src/eval.rs
+++ src/eval.rs
@@ -1,8 +1,9 @@
 use rune::{
-    termcolor::{ColorChoice, StandardStream},
-    Diagnostics, Vm,
+    termcolor::{ColorChoice, StandardStream}, Diagnostics, Source, Sources, Vm
 };
+use tracing::debug;
 use std::{path::Path, sync::Arc};
+
 
 use crate::Result;
 
@@ -12,13 +13,16 @@ pub async fn eval<P: AsRef<Path>>(manifest: P) -> Result<()> {
     let context = rune_modules::default_context()?;
     let runtime = Arc::new(context.runtime()?);
 
-    let mut sources = rune::sources! {
-        entry => {
-            async fn main(timeout) {
-                time::delay_for(time::Duration::from_secs(timeout)).await
-            }
-        }
-    };
+    // let mut sources = rune::sources! {
+    //     entry => {
+    //         async fn main(timeout) {
+    //             time::delay_for(time::Duration::from_secs(timeout)).await
+    //         }
+    //     }
+    // };
+    let source = Source::from_path(manifest)?;
+    let mut sources = Sources::new();
+    sources.insert(source)?;
 
     let mut diagnostics = Diagnostics::new();
 
@@ -35,6 +39,9 @@ pub async fn eval<P: AsRef<Path>>(manifest: P) -> Result<()> {
     let unit = result?;
 
     let vm = Vm::new(runtime, Arc::new(unit));
+    let res = vm.send_execute(["mk_nodes"], ())?.async_complete().await.unwrap();
+
+    debug!(?res);
 
     Ok(())
 }
