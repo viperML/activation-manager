21386875770198a595f9ff5437528a82ecf337f3
Fernando Ayats <ayatsfer@gmail.com>
2024-04-21 19:20:22 +0200

    function returns

--- src/eval.rs
+++ src/eval.rs
@@ -1,28 +1,40 @@
+use core::fmt::{Debug, Formatter};
 use rune::{
-    termcolor::{ColorChoice, StandardStream}, Diagnostics, Source, Sources, Vm
+    alloc::clone::TryClone,
+    runtime::{Function, Shared},
+    termcolor::{ColorChoice, StandardStream},
+    Any, Diagnostics, Module, Source, Sources, Value, Vm,
 };
+use std::{any::type_name, borrow::Borrow, path::Path, sync::Arc};
+use tokio::runtime::Runtime;
 use tracing::debug;
-use std::{path::Path, sync::Arc};
-
 
 use crate::Result;
 
-pub async fn eval<P: AsRef<Path>>(manifest: P) -> Result<()> {
-    let manifest = manifest.as_ref();
+#[derive(Debug, Any)]
+#[rune(constructor)]
+pub struct Node {
+    #[rune(get, set)]
+    name: String,
+    #[rune(get, set)]
+    before: Vec<String>,
+    #[rune(get, set)]
+    after: Vec<String>,
+    #[rune(get, set)]
+    action: Shared<Function>,
+}
 
-    let context = rune_modules::default_context()?;
+pub async fn eval<P: AsRef<Path>>(manifest: P) -> Result<()> {
+    let mut context = rune_modules::default_context()?;
+
+    let mut module = Module::new();
+    module.ty::<Node>()?;
+    context.install(module)?;
+
     let runtime = Arc::new(context.runtime()?);
 
-    // let mut sources = rune::sources! {
-    //     entry => {
-    //         async fn main(timeout) {
-    //             time::delay_for(time::Duration::from_secs(timeout)).await
-    //         }
-    //     }
-    // };
-    let source = Source::from_path(manifest)?;
     let mut sources = Sources::new();
-    sources.insert(source)?;
+    sources.insert(Source::from_path(manifest)?)?;
 
     let mut diagnostics = Diagnostics::new();
 
@@ -39,9 +51,25 @@ pub async fn eval<P: AsRef<Path>>(manifest: P) -> Result<()> {
     let unit = result?;
 
     let vm = Vm::new(runtime, Arc::new(unit));
-    let res = vm.send_execute(["mk_nodes"], ())?.async_complete().await.unwrap();
 
-    debug!(?res);
+    let res = vm
+        .try_clone()?
+        .send_execute(["mk_nodes"], ())?
+        .async_complete()
+        .await
+        .unwrap();
+
+    let nodes: Vec<Node> = rune::from_value(res)?;
+    debug!(?nodes);
+
+    for node in nodes {
+        let f = node.action.take()?;
+        debug!(?f);
+
+        let vm = vm.try_clone()?;
+        let fut = f.async_send_call(());
+        let res: Value = fut.await.unwrap();
+    }
 
     Ok(())
 }
