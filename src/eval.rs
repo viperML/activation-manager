fb0eece4c1e7a49eabe80e4d6740e56d1c2f4731
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-04-24 11:02:53 +0200

    sync nodes

--- src/eval.rs
+++ src/eval.rs
@@ -1,9 +1,9 @@
 use core::fmt::{Debug, Formatter};
 use rune::{
     alloc::clone::TryClone,
-    runtime::{Function, Shared, SyncFunction},
+    runtime::{Function, Shared, Struct, SyncFunction, VmError, VmResult},
     termcolor::{ColorChoice, StandardStream},
-    Any, Diagnostics, Module, Source, Sources, Value, Vm,
+    Any, Diagnostics, FromValue, Hash, Module, Source, Sources, Value, Vm,
 };
 use std::{any::type_name, borrow::Borrow, path::Path, sync::Arc, time::Duration};
 use tokio::runtime::Runtime;
@@ -11,24 +11,62 @@ use tracing::debug;
 
 use crate::Result;
 
-#[derive(Debug, Any)]
-#[rune(constructor)]
-pub struct Node {
-    #[rune(get, set)]
+#[derive(Debug)]
+pub struct Node
+// manually mark them for me to remember
+where
+    Self: Send + Sync,
+{
     name: String,
-    #[rune(get, set)]
-    before: Vec<String>,
-    #[rune(get, set)]
-    after: Vec<String>,
-    #[rune(get, set)]
-    action: Shared<Function>,
+    // before: Vec<String>,
+    // after: Vec<String>,
+    action: Hash,
+}
+
+impl FromValue for Node {
+    fn from_value(value: Value) -> VmResult<Self> {
+        if let Value::Struct(s) = value {
+            let mut s = s.borrow_mut().unwrap();
+
+            let name: String = get_owned(&mut s, "name").unwrap();
+
+            let action: SyncFunction = rune::from_value(std::mem::take(
+                s.get_mut("action")
+                    .ok_or_else(|| VmError::panic("failed to get key"))
+                    .unwrap(),
+            ))
+            .unwrap();
+
+            let h = action.type_hash();
+
+            VmResult::Ok(Self { name, action: h })
+        } else {
+            todo!();
+        }
+    }
+}
+
+/// Gets an element from an struct by replacing it with the empty tuple
+fn get_owned<T, S>(r#struct: &mut Struct, name: S) -> Result<T, VmError>
+where
+    T: rune::FromValue,
+    S: AsRef<str>,
+{
+    let name = name.as_ref();
+
+    let refmut = r#struct
+        .get_mut(name)
+        .ok_or_else(|| VmError::panic("failed to get key"))?;
+    let value = std::mem::take(refmut);
+
+    rune::from_value(value)
 }
 
 pub async fn eval<P: AsRef<Path>>(manifest: P) -> Result<()> {
     let mut context = rune_modules::default_context()?;
 
     let mut module = Module::new();
-    module.ty::<Node>()?;
+    // module.ty::<Node>()?;
     context.install(module)?;
 
     let runtime = Arc::new(context.runtime()?);
@@ -63,15 +101,33 @@ pub async fn eval<P: AsRef<Path>>(manifest: P) -> Result<()> {
     debug!(?nodes);
 
     for node in nodes {
-        let f = node.action.take()?.into_sync().unwrap();
-        let exec = vm.try_clone()?.send_execute(f.type_hash(), ())?;
-        let res = tokio::spawn(async {
+        debug!(?node);
+
+        let exec = vm.try_clone()?.send_execute(node.action, ())?;
+        let res = exec.async_complete().await.unwrap();
+        let subnodes: Vec<Node> = rune::from_value(res)?;
+
+        for subnode in subnodes {
+            let exec = vm.try_clone()?.send_execute(subnode.action, ())?;
             let res = exec.async_complete().await.unwrap();
-            println!("done");
-        });
+        }
+
+        // let f = node.action.take()?.into_sync().unwrap();
+        // let exec = vm.try_clone()?.send_execute(f.type_hash(), ())?;
+        // let hash = tokio::spawn(async {
+        //     let res = exec.async_complete().await.unwrap();
+        //     let f = Function::from_value(res).unwrap();
+        //     let hash = f.type_hash();
+        //     hash
+        // })
+        // .await
+        // .unwrap();
+
+        // let exec2 = vm.try_clone()?.send_execute(hash, ())?;
+        // exec2.async_complete().await.unwrap();
     }
 
-    tokio::time::sleep(Duration::from_secs(3)).await;
+    // tokio::time::sleep(Duration::from_secs(3)).await;
 
     Ok(())
 }
