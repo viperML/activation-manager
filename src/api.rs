e583779014c220bca122ec2e8f02729495ee931e
Fernando Ayats <ayatsfer@gmail.com>
2024-06-08 19:07:26 +0200

    subcommand

--- src/api.rs
+++ src/api.rs
@@ -1,9 +1,13 @@
-use rune::Module;
+use rune::{
+    runtime::{VmError, VmResult},
+    vm_try, Module,
+};
 
 pub(crate) fn module() -> eyre::Result<Module> {
     let mut m = Module::new();
 
     m.function_meta(debug)?;
+    m.function_meta(run)?;
 
     Ok(m)
 }
@@ -11,4 +15,22 @@ pub(crate) fn module() -> eyre::Result<Module> {
 #[rune::function]
 fn debug(message: String) {
     tracing::debug!("{}", message);
+}
+
+#[rune::function]
+async fn run(command: String) -> VmResult<()> {
+    let mut args = command.split_whitespace();
+    let argv0 = vm_try!(args.next().ok_or_else(|| VmError::panic("FIXME")));
+
+    let mut cmd = tokio::process::Command::new(argv0);
+    cmd.args(args);
+
+    let mut child = vm_try!(cmd.spawn().map_err(|_| VmError::panic("FIXME")));
+
+    let res = vm_try!(child.wait().await.map_err(|_| VmError::panic("FIXME")));
+
+    match res.success() {
+        true => VmResult::Ok(()),
+        false => VmResult::panic("FIXME"),
+    }
 }
