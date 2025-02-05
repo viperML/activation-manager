09a354ad61efc81c9809f0f9f801fd71ed8cf4cb
Fernando Ayats <ayatsfer@gmail.com>
2025-02-05 17:12:43 +0100

    First commit

---
+++ shell.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> {};
+pkgs.mkShell {
+  packages = [
+    pkgs.cargo
+    pkgs.rustc
+
+    pkgs.rust-analyzer
+    pkgs.rustfmt
+
+    # If the dependencies need system libs, you usually need pkg-config + the lib
+    pkgs.pkg-config
+    lua54Packages.lua
+    lua-language-server
+  ];
+
+  env = {
+    RUST_BACKTRACE = "full";
+  };
+}
+
