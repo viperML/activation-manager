1d31be4271c91b985c3a8591e196e7d52abd70aa
Fernando Ayats <ayatsfer@gmail.com>
2025-02-16 11:22:38 +0100

    Fix warns

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@
 
 mod api;
 mod exec;
-mod node;
-mod gsettings;
 mod exec_node;
+mod gsettings;
+mod node;
 
 fn main() -> eyre::Result<()> {
+    use std::str::FromStr;
     use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+    let level = std::env::var("AM_LOG").unwrap_or(String::from("warn"));
 
     tracing_subscriber::registry()
         .with(fmt::layer().without_time())
-        .with(EnvFilter::from_env("AM_LOG"))
+        .with(EnvFilter::from_str(&level)?)
         .init();
+
+    tracing::trace!(?level);
 
     color_eyre::install()?;
 
